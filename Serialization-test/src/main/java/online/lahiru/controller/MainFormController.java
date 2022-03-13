6a714383f3f99975020783651d2810b32976a23c
LahiruRJ <sanlahiru720@gmail.com>
2022-03-13 10:29:34 +0530

    Set mainform UI

---
+++ Serialization-test/src/main/java/online/lahiru/controller/MainFormController.java
@@ -0,0 +1,21 @@
+package online.lahiru.controller;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableView;
+
+public class MainFormController {
+    public com.jfoenix.controls.JFXTextField txtId;
+    public com.jfoenix.controls.JFXTextField txtName;
+    public com.jfoenix.controls.JFXTextField txtAddress;
+    public com.jfoenix.controls.JFXTextField txtPicture;
+    public JFXButton btnBrowse;
+    public TableView tblView;
+    public JFXButton btnNew;
+
+    public void btnNew_OnAction(ActionEvent event) {
+    }
+
+    public void btnBrowse_OnAction(ActionEvent event) {
+    }
+}
